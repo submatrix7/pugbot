1aa2605b0f6134d0e6fedaf34522a6c88189a980
submatrix7 <submatrix7@gmail.com>
2017-03-15 12:33:32 -0400

    Update mythic.py

--- commands/mythic.py
+++ commands/mythic.py
@@ -123,7 +123,7 @@ def get_char(name, server, target_region, api_key):
 
     # iLvL
     return_string += "Equipped Item Level: **%s**\n" % equipped_ivl
-    return_string += "Average Item Level: %s \n\n" % average_ilvl
+    return_string += "Average Item Level: *%s* \n\n" % average_ilvl
 
     # Mythic Progression
     return_string += "Mythics Completed\n +2: %s\n +5: %s\n+10: %s\n+15: %s\n\n" % (mythic_progress["plus_two"],
