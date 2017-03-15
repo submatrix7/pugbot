d734fd1da56627ba572235009d8645f53559c6fb
submatrix7 <submatrix7@gmail.com>
2017-03-15 12:54:20 -0400

    format testing

--- commands/mythic.py
+++ commands/mythic.py
@@ -119,21 +119,25 @@ def get_char(name, server, target_region, api_key):
     return_string = ''
     return_string += "**%s** - **%s** - **%s %s**\n" % (
         name.title(), server.title(), player_dict['level'], class_dict[player_dict['class']])
+    
+    return_string += "|character|server|level|class||---|---|---|---||**%s**|**%s**|**%s|%s**|\n" % (
+        name.title(), server.title(), player_dict['level'], class_dict[player_dict['class']])
+    
     #return_string += '```CSS\n'  # start Markdown
 
     # iLvL
-    return_string += "Equipped Item Level: **%s**\n" % equipped_ivl
-    return_string += "Average Item Level: *%s* \n\n" % average_ilvl
+    return_string += "**Equipped Item Level**: %s\n" % equipped_ivl
+    return_string += "**Average Item Level**: %s \n\n" % average_ilvl
 
     # Mythic Progression
-    return_string += "Mythics Completed\n +2: %s\n +5: %s\n+10: %s\n+15: %s\n\n" % (mythic_progress["plus_two"],
-                                                                                    mythic_progress["plus_five"],
-                                                                                    mythic_progress["plus_ten"],
-                                                                                    mythic_progress["plus_fifteen"])
+    return_string += "**Mythics Completed**\n **+2**: %s\n **+5**: %s\n**+10**: %s\n**+15**: %s\n\n" % (
+        mythic_progress["plus_two"],
+        mythic_progress["plus_five"],
+        mythic_progress["plus_ten"],
+        mythic_progress["plus_fifteen"])
 
     # Gems
-    return_string += "Gems Equipped: %s/%s\n" % (
-        sockets["equipped_gems"], sockets["total_sockets"])
+    return_string += "Gems Equipped: %s/%s\n" % (sockets["equipped_gems"], sockets["total_sockets"])
 
     # Enchants
     return_string += "Enchants: %s/%s\n" % (enchants["enchantable_slots"] - enchants["total_missing"],
