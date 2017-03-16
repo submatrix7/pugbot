30d990fec273c35119e97fef2c4be583bb6699ae
submatrix7 <submatrix7@gmail.com>
2017-03-16 09:40:30 -0400

    Update kills.py

--- commands/kills.py
+++ commands/kills.py
@@ -48,7 +48,7 @@ def get_raid_progression(player_dictionary, raid):
     raid_progress = {}
     for raid in cRAID:
         raid_name = raid[0]
-        raid_progress[raid_name] = 'progress': get_raid_progression(player_dict, raid_name)
+        raid_progress[raid_name] = ['progress'] get_raid_progression(player_dict, raid_name)
 
     return_string = ''
     return_string += "**%s** - **%s** - **%s %s**\n" % (
