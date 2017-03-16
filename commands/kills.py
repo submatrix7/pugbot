13e9ea5a7e79d6ae3f6b33fd9b27ee73a0ab9284
submatrix7 <submatrix7@gmail.com>
2017-03-16 07:56:00 -0400

    Update kills.py

--- commands/kills.py
+++ commands/kills.py
@@ -27,11 +27,11 @@ def get_char(name, server, target_region, api_key):
 
     #Test
     def get_raid_progression(player_dictionary, raid):
-    r = [x for x in player_dictionary["progression"]
-    ["raids"] if x["name"] in raid][0]
-    nkills = 0
-    hkills = 0
-    mkills = 0
+        r = [x for x in player_dictionary["progression"]
+        ["raids"] if x["name"] in raid][0]
+        nkills = 0
+        hkills = 0
+        mkills = 0
     
     boss_list = {}
     for boss in r["bosses"]:
